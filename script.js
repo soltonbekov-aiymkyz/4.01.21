a7fc4f709861044ccf73c96764ed316be9b268f1
Your Name <you@example.com>
2021-02-07 23:30:04 -0800

    added firle

--- script.js
+++ script.js
@@ -4,3 +4,12 @@
   
  let btnSubstrack = document.querySelector('#substrack');
  
+ let input = document.querySelector('input');
+ 
+ btnAdd.addEventListener('click',   ()=>{
+input.value = parseInt(input.value) + 1;
+ })
+
+ btnSubstrack.addEventListener('click',   ()=>{
+    input.value = parseInt(input.value) - 1; 
+})
